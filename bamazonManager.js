c4cfc60040ea33d28cbc385592cace7211d3af2c
William Brooks <brooks.william@gmail.com>
2017-11-11 16:14:44 -0800

    all 3 parts working

--- bamazonManager.js
+++ bamazonManager.js
@@ -60,8 +60,8 @@ var connection = mysql.createConnection({
 // connect to the database
 connection.connect(function(err) {
     if (err) throw err;
+    //go to welcome screen/user prompt
     managerStart();
-    //  connection.end();
 });
 
 
@@ -72,7 +72,6 @@ connection.connect(function(err) {
 // Display prompt with list of 'Manager' options
 //-----------------------------
 function managerStart() {
-
     inquirer.prompt({
         type: 'list',
         name: 'mode',
@@ -94,12 +93,14 @@ function managerStart() {
                 addProduct();
                 break;
             default:
+                //go to welcome screen/user prompt
                 managerStart();
         }
     });
 }
 
-
+// Display a table of prodcts with list of 'Manager' options
+//-----------------------------
 function displayProducts() {
     connection.query("SELECT * FROM products", function(err, result) {
         if (err) {
@@ -127,48 +128,79 @@ function displayProducts() {
 
         str1 = t1.render();
         console.log(str1);
+        //go to welcome screen/user prompt
         managerStart();
     });
 
 }
 
+// Display prompt with list of 'Manager' options
+//-----------------------------
 function displayLowInventory() {
-    connection.query("SELECT * FROM products Where stock_quantity < '10'", function(err, result) {
-        if (err) {
-            throw err;
-        }
-        var rows = [];
-        for (var i = 0; i < result.length; i++) {
-            var row = [];
-            row.push(result[i].id);
-            row.push(result[i].product_name);
-            row.push("$" + result[i].price);
-            row.push(result[i].stock_quantity);
-            rows.push(row);
-        }
 
-        var t1 = Table(itemsHeader, rows, {
-            borderStyle: 1,
-            borderColor: "red",
-            paddingBottom: 0,
-            headerAlign: "center",
-            align: "center",
-            color: "white",
-            truncate: "..."
+    // prompt for info about the item being added
+    inquirer
+        .prompt({
+            name: "stockAmount",
+            type: "input",
+            message: "You will be shown all products with stock lower than (enter number):"
+        }).then(function(item) {
+            connection.query("SELECT * FROM products Where stock_quantity < ?", item.stockAmount, function(err, result) {
+                if (err) {
+                    throw err;
+                }
+                var rows = [];
+                for (var i = 0; i < result.length; i++) {
+                    var row = [];
+                    row.push(result[i].id);
+                    row.push(result[i].product_name);
+                    row.push("$" + result[i].price);
+                    row.push(result[i].stock_quantity);
+                    rows.push(row);
+                }
+
+                var t1 = Table(itemsHeader, rows, {
+                    borderStyle: 1,
+                    borderColor: "red",
+                    paddingBottom: 0,
+                    headerAlign: "center",
+                    align: "center",
+                    color: "white",
+                    truncate: "..."
+                });
+
+                var str1 = t1.render();
+                console.log(str1);
+                //go to welcome screen/user prompt
+                managerStart();
+            });
+
         });
 
-        var str1 = t1.render();
-        console.log(str1);
-        managerStart();
-    });
 }
 
 
 // function to handle posting new items up for auction
 function addProduct() {
 
-    //GET THE DEPARTMENT CHOICES FROM THE DEPARTMENT TABLE
+    //TODO GET THE DEPARTMENT CHOICES FROM THE DEPARTMENT TABLE
 
+    var deptChoices = [];
+    var deptLookup = [];
+
+    connection.query("SELECT department_id, department_name FROM departments", function(err, result) {
+        if (err) {
+            throw err;
+        }
+        for (var i = 0; i < result.length; i++) {
+            var deptRow = [];
+            deptRow.push(result[i].department_id);
+            deptRow.push(result[i].department_name);
+            deptLookup.push(deptRow);
+            deptChoices.push(result[i].department_name);
+        }
+
+    });
 
     // prompt for info about the item being added
     inquirer
@@ -181,7 +213,7 @@ function addProduct() {
                 name: "department",
                 type: "list",
                 message: "What department would you like to place your item in?",
-                choices: ["video Games", "Food and Drink", "Apparel", "Camping"]
+                choices: deptChoices
             },
             {
                 name: "price",
@@ -196,7 +228,24 @@ function addProduct() {
         ])
         .then(function(item) {
 
-            connection.query("INSERT INTO products SET product_name=?,  department_name=?, price=?, stock_quantity=?", [item.name, item.department, item.price, item.stockQuantity], function(err) {
+            var deptID;
+            var deptFound = false;
+
+            //lookup the department ID that matches the department name selected
+            for (var i = 0; i < deptLookup.length; i++) {
+                if (item.department === deptLookup[i][1]) {
+                    deptID = deptLookup[i][0];
+                    deptFound = true;
+                }
+            }
+            //display an error in the extremely unlikely event a department is not found
+            if (!deptFound) {
+                console.log("No Department Found, Try Again");
+                managerStart();
+            }
+
+            //add the product to the table with the department ID found from above
+            connection.query("INSERT INTO products SET product_name=?,  department_id=?, price=?, stock_quantity=?", [item.name, deptID, item.price, item.stockQuantity], function(err) {
                 if (err) throw err;
                 console.log("Item Added");
                 managerStart();
@@ -206,124 +255,85 @@ function addProduct() {
 
 function addInventory() {
     connection.query("SELECT * FROM products", function(err, result) {
-        if (err) {
-            throw err;
-        }
-        var rows = [];
-        for (var i = 0; i < result.length; i++) {
-            var row = [];
-            row.push(result[i].id);
-            row.push(result[i].product_name);
-            row.push("$" + result[i].price);
-            row.push(result[i].stock_quantity);
-            rows.push(row);
-        }
+            if (err) {
+                throw err;
+            }
+            var rows = [];
+            for (var i = 0; i < result.length; i++) {
+                var row = [];
+                row.push(result[i].id);
+                row.push(result[i].product_name);
+                row.push("$" + result[i].price);
+                row.push(result[i].stock_quantity);
+                rows.push(row);
+            }
 
-        var t1 = Table(itemsHeader, rows, {
-            borderStyle: 1,
-            borderColor: "blue",
-            paddingBottom: 0,
-            headerAlign: "center",
-            align: "center",
-            color: "white",
-            truncate: "..."
-        });
+            var t1 = Table(itemsHeader, rows, {
+                borderStyle: 1,
+                borderColor: "blue",
+                paddingBottom: 0,
+                headerAlign: "center",
+                align: "center",
+                color: "white",
+                truncate: "..."
+            });
 
-        var str1 = t1.render();
-        console.log(str1);
+            var str1 = t1.render();
+            console.log(str1);
+
+            var maxID; //get the last ID number
 
 
-        // prompt for info about the item  to add inventory to
-        inquirer.prompt([{
-                    name: "id",
-                    type: "input",
-                    message: "What is the item ID?"
-                },
-                {
-                    name: "stockQuantity",
-                    type: "input",
-                    message: "What is amount of stock to add quantity?",
+            connection.query("SELECT id FROM products ORDER BY id DESC LIMIT 1", function(err, result) {
+                if (err) {
+                    throw err;
                 }
-            ])
-            .then(function(item) {
+                maxID = result[0].id;
 
-                var newStock;
-                //check the stock
-                connection.query("SELECT stock_quantity FROM products WHERE ?", { id: item.id }, function(err, result) {
-                    if (err) {
-                        throw err;
+            });
+
+            // prompt for info about the item  to add inventory to
+            inquirer.prompt([{
+                name: "id",
+                type: "input",
+                message: "What is the item ID?",
+                //check for a valid item number
+                validate: function(value) {
+                    //check for a valid ID
+                    if (value > 0 && value <= maxID) {
+                        return true;
                     }
+                    return false;
+                }
+            }, {
+                name: "stockQuantity",
+                type: "input",
+                message: "What is the amount of stock you want to add?",
+            }])
+        .then(function(item) {
 
-                    //record the current stock level to a local variable
-                    var newStock = parseInt(result[0].stock_quantity) + parseInt(item.stockQuantity);
+            var newStock;
 
-                    connection.query("UPDATE products SET stock_quantity=? WHERE id=?", [newStock, item.id], function(err) {
-                        if (err) throw err;
-                        console.log("Item Added");
-                        managerStart();
-                    });
+            //check the stock
+            connection.query("SELECT stock_quantity FROM products WHERE ?", { id: item.id }, function(err, result) {
+                if (err) {
+                    throw err;
+                }
+
+                //add the current stock from database to the newly added amount
+                var newStock = parseInt(result[0].stock_quantity) + parseInt(item.stockQuantity);
+
+                //put the new total stock into the database
+                connection.query("UPDATE products SET stock_quantity=? WHERE id=?", [newStock, item.id], function(err) {
+                    if (err) throw err;
+                    console.log("Item Added");
+                    //go to welcome screen/user prompt
+                    managerStart();
                 });
             });
+        });
     });
 }
-
-
-
-
-
-
-
-// // Check if enough stock to place order
-// //-----------------------------
-// function placeOrder(chosenID, quantityOrdered) {
-//     var currentStock = 0;
-//     //check the stock
-//     connection.query("SELECT stock_quantity FROM products WHERE ?", { id: chosenID }, function(err, result) {
-//         if (err) {
-//             throw err;
-//         }
-
-//         //record the current stock level to a local variable
-//         currentStock = result[0].stock_quantity;
-
-//         //check if enough stock
-//         if (quantityOrdered > currentStock) {
-//             console.log("Not Enough Stock, We only have", currentStock, "in warehouse");
-//             managerStart();
-//         }
-//         //if enough go ahead with order
-//         else {
-//             //update database to reflect the remaining quantity
-//             adjustStock(chosenID, quantityOrdered, currentStock);
-//         }
-//     });
-// }
-
-// // adjust the stock remaining in database
-// //-----------------------------
-// function adjustStock(chosenID, quantityOrdered, currentStock) {
-//     var remainingStock = (currentStock - quantityOrdered);
-//     connection.query("UPDATE products SET stock_quantity=? WHERE id=?", [remainingStock, chosenID], function(err) {
-//         if (err) throw err;
-//         console.log("Stock Updated");
-//     });
-//     //Once the update goes through, show the customer the total cost of their purchase
-//     calculateOrderTotal(chosenID, quantityOrdered);
-// }
-
-
-// // Calculate the total price of the order
-// //-----------------------------
-// function calculateOrderTotal(chosenID, quantityOrdered) {
-//     console.log('calculateOrderTotal');
-//     connection.query("SELECT price FROM products WHERE ?", { id: chosenID }, function(err, result) {
-//         if (err) throw err;
-//         var totalPrice = result[0].price * quantityOrdered;
-//         console.log("Total Cost of Purchase: $" + totalPrice)
-//         managerStart();
-//     });
-
-// }
 
 
 
